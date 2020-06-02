1856eba2147814f6ecbfc71dbcdae543f834e069
liudehua <liudehua@vchangyi.com>
2020-06-02 15:40:37 +0800

    测试

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,45 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div>{{ obj.name }}</div>
+    <div>{{user}}</div>
+    <button @click="update">点击一下</button>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+// import HelloWorld from "@/components/HelloWorld.vue";
+import { reactive, ref } from "vue";
 
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
+  name: "Home",
+  // components: {
+  //   HelloWorld
+  // },
+  setup() {
+    console.log("初始化前");
+    const obj = reactive({
+      name: "frist vue demo"
+    });
+    const user = ref(1);
+
+    const update = () => {
+      user.value = user.value + 1;
+    };
+
+    // setInterval(() => {
+    //   update();
+    // }, 1000);
+    return {
+      user: user,
+      update
+    };
+  },
+  beforeMount() {
+    console.log("dom挂在前");
+  },
+  mounted() {
+    console.log("dom挂在后");
   }
-}
+};
 </script>
